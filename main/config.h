586794ec4d811eee5015619bae3c3c56f8996216
VietDzung <dzungpv@outlook.com>
2023-11-30 23:47:23 +0700

    Set version for release

--- main/config.h
+++ main/config.h
@@ -116,7 +116,7 @@ unsigned long requestWifiScanTime = 0;
 #define WIFI_SCAN_PERIOD 120000
 unsigned lastWifiScanMillis;
 
-const PROGMEM char *m2mqtt_version = "2023.11.4";
+const PROGMEM char *m2mqtt_version = "2023.11.5";
 
 // Define global variables for files
 #ifdef ESP32
